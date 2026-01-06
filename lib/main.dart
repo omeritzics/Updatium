a3834ecec627a5279840107ea3b4b71302537bd9
Omer I.S. <omeritzicschwartz@gmail.com>
2026-01-06 13:37:24 +0200

    Convert translation files to PO

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,8 @@ import 'package:easy_localization/src/easy_localization_controller.dart';
 // ignore: implementation_imports
 import 'package:easy_localization/src/localization.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
+// Added for PO file support
+import 'package:easy_gettext/easy_gettext.dart';
 
 List<MapEntry<Locale, String>> supportedLocales = const [
   MapEntry(Locale('en'), 'English'),
@@ -28,7 +30,7 @@ List<MapEntry<Locale, String>> supportedLocales = const [
   MapEntry(Locale('ja'), '日本語'),
   MapEntry(Locale('hu'), 'Magyar'),
   MapEntry(Locale('de'), 'Deutsch'),
-  MapEntry(Locale('fa'), 'فارسی'),
+  MapEntry(Locale('fa'), 'فارסי'),
   MapEntry(Locale('fr'), 'Français'),
   MapEntry(Locale('es'), 'Español'),
   MapEntry(Locale('pl'), 'Polski'),
@@ -70,7 +72,8 @@ Future<void> loadTranslations() async {
     forceLocale: forceLocale,
     fallbackLocale: fallbackLocale,
     supportedLocales: supportedLocales.map((e) => e.key).toList(),
-    assetLoader: const RootBundleAssetLoader(),
+    // Updated to use GettextLoader for PO files
+    assetLoader: const GettextLoader(),
     useOnlyLangCode: false,
     useFallbackTranslations: true,
     path: localeDir,
@@ -160,264 +163,4 @@ void main() async {
       child: EasyLocalization(
         supportedLocales: supportedLocales.map((e) => e.key).toList(),
         path: localeDir,
-        fallbackLocale: fallbackLocale,
-        useOnlyLangCode: false,
-        child: const Updatium(),
-      ),
-    ),
-  );
-  BackgroundFetch.registerHeadlessTask(backgroundFetchHeadlessTask);
-}
-
-class Updatium extends StatefulWidget {
-  const Updatium({super.key});
-
-  @override
-  State<Updatium> createState() => _UpdatiumState();
-}
-
-class _UpdatiumState extends State<Updatium> {
-  var existingUpdateInterval = -1;
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      requestNonOptionalPermissions();
-    });
-  }
-
-  Future<void> requestNonOptionalPermissions() async {
-    final NotificationPermission notificationPermission =
-        await FlutterForegroundTask.checkNotificationPermission();
-    if (notificationPermission != NotificationPermission.granted) {
-      await FlutterForegroundTask.requestNotificationPermission();
-    }
-    if (!await FlutterForegroundTask.isIgnoringBatteryOptimizations) {
-      await FlutterForegroundTask.requestIgnoreBatteryOptimization();
-    }
-  }
-
-  void initForegroundService() {
-    // ignore: invalid_use_of_visible_for_testing_member
-    if (!FlutterForegroundTask.isInitialized) {
-      FlutterForegroundTask.init(
-        androidNotificationOptions: AndroidNotificationOptions(
-          channelId: 'bg_update',
-          channelName: tr('foregroundService'),
-          channelDescription: tr('foregroundService'),
-          onlyAlertOnce: true,
-        ),
-        iosNotificationOptions: const IOSNotificationOptions(
-          showNotification: false,
-          playSound: false,
-        ),
-        foregroundTaskOptions: ForegroundTaskOptions(
-          eventAction: ForegroundTaskEventAction.repeat(900000),
-          autoRunOnBoot: true,
-          autoRunOnMyPackageReplaced: true,
-          allowWakeLock: true,
-          allowWifiLock: true,
-        ),
-      );
-    }
-  }
-
-  Future<ServiceRequestResult?> startForegroundService(bool restart) async {
-    initForegroundService();
-    if (await FlutterForegroundTask.isRunningService) {
-      if (restart) {
-        return FlutterForegroundTask.restartService();
-      }
-    } else {
-      return FlutterForegroundTask.startService(
-        serviceTypes: [ForegroundServiceTypes.specialUse],
-        serviceId: 666,
-        notificationTitle: tr('foregroundService'),
-        notificationText: tr('fgServiceNotice'),
-        notificationIcon: NotificationIcon(
-          metaDataName: 'com.omeritzics.updatium.service.NOTIFICATION_ICON',
-        ),
-        callback: startCallback,
-      );
-    }
-    return null;
-  }
-
-  stopForegroundService() async {
-    if (await FlutterForegroundTask.isRunningService) {
-      return FlutterForegroundTask.stopService();
-    }
-  }
-
-  // void onReceiveForegroundServiceData(Object data) {
-  //   print('onReceiveTaskData: $data');
-  // }
-
-  @override
-  void dispose() {
-    // Remove a callback to receive data sent from the TaskHandler.
-    // FlutterForegroundTask.removeTaskDataCallback(onReceiveForegroundServiceData);
-    super.dispose();
-  }
-
-  Future<void> initPlatformState() async {
-    await BackgroundFetch.configure(
-      BackgroundFetchConfig(
-        minimumFetchInterval: 15,
-        stopOnTerminate: false,
-        startOnBoot: true,
-        enableHeadless: true,
-        requiresBatteryNotLow: false,
-        requiresCharging: false,
-        requiresStorageNotLow: false,
-        requiresDeviceIdle: false,
-        requiredNetworkType: NetworkType.ANY,
-      ),
-      (String taskId) async {
-        await bgUpdateCheck(taskId, null);
-        BackgroundFetch.finish(taskId);
-      },
-      (String taskId) async {
-        context.read<LogsProvider>().add('BG update task timed out.');
-        BackgroundFetch.finish(taskId);
-      },
-    );
-    if (!mounted) return;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    SettingsProvider settingsProvider = context.watch<SettingsProvider>();
-    AppsProvider appsProvider = context.read<AppsProvider>();
-    LogsProvider logs = context.read<LogsProvider>();
-    NotificationsProvider notifs = context.read<NotificationsProvider>();
-    if (settingsProvider.updateInterval == 0) {
-      stopForegroundService();
-      BackgroundFetch.stop();
-    } else {
-      if (settingsProvider.useFGService) {
-        BackgroundFetch.stop();
-        startForegroundService(false);
-      } else {
-        stopForegroundService();
-        BackgroundFetch.start();
-      }
-    }
-    if (settingsProvider.prefs == null) {
-      settingsProvider.initializeSettings();
-    } else {
-      bool isFirstRun = settingsProvider.checkAndFlipFirstRun();
-      if (isFirstRun) {
-        logs.add('This is the first ever run of Updatium.');
-        // If this is the first run, add Updatium to the Apps list
-        if (!fdroid) {
-          getInstalledInfo(updatiumId)
-              .then((value) {
-                if (value?.versionName != null) {
-                  appsProvider.saveApps([
-                    App(
-                      updatiumId,
-                      updatiumUrl,
-                      'omeritzics',
-                      'Updatium',
-                      value!.versionName,
-                      value.versionName!,
-                      [],
-                      0,
-                      {
-                        'versionDetection': true,
-                        'apkFilterRegEx': 'fdroid',
-                        'invertAPKFilter': true,
-                      },
-                      null,
-                      false,
-                    ),
-                  ], onlyIfExists: false);
-                }
-              })
-              .catchError((err) {
-                print(err);
-              });
-        }
-      }
-      if (!supportedLocales.map((e) => e.key).contains(context.locale) ||
-          (settingsProvider.forcedLocale == null &&
-              context.deviceLocale != context.locale)) {
-        settingsProvider.resetLocaleSafe(context);
-      }
-    }
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      notifs.checkLaunchByNotif();
-    });
-
-    return WithForegroundTask(
-      child: DynamicColorBuilder(
-        builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
-          // Decide on a colour/brightness scheme based on OS and user settings
-          ColorScheme lightColorScheme;
-          ColorScheme darkColorScheme;
-          if (lightDynamic != null &&
-              darkDynamic != null &&
-              settingsProvider.useMaterialYou) {
-            lightColorScheme = lightDynamic.harmonized();
-            darkColorScheme = darkDynamic.harmonized();
-          } else {
-            lightColorScheme = ColorScheme.fromSeed(
-              seedColor: settingsProvider.themeColor,
-            );
-            darkColorScheme = ColorScheme.fromSeed(
-              seedColor: settingsProvider.themeColor,
-              brightness: Brightness.dark,
-            );
-          }
-
-          // set the background and surface colors to pure black in the amoled theme
-          if (settingsProvider.useBlackTheme) {
-            darkColorScheme = darkColorScheme
-                .copyWith(surface: Colors.black)
-                .harmonized();
-          }
-
-          if (settingsProvider.useSystemFont) NativeFeatures.loadSystemFont();
-
-          return MaterialApp(
-            title: 'Updatium',
-            localizationsDelegates: context.localizationDelegates,
-            supportedLocales: context.supportedLocales,
-            locale: context.locale,
-            navigatorKey: globalNavigatorKey,
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              useMaterial3: true,
-              colorScheme: settingsProvider.theme == ThemeSettings.dark
-                  ? darkColorScheme
-                  : lightColorScheme,
-              fontFamily: settingsProvider.useSystemFont
-                  ? 'SystemFont'
-                  : 'Montserrat',
-            ),
-            darkTheme: ThemeData(
-              useMaterial3: true,
-              colorScheme: settingsProvider.theme == ThemeSettings.light
-                  ? lightColorScheme
-                  : darkColorScheme,
-              fontFamily: settingsProvider.useSystemFont
-                  ? 'SystemFont'
-                  : 'Montserrat',
-            ),
-            home: Shortcuts(
-              shortcuts: <LogicalKeySet, Intent>{
-                LogicalKeySet(LogicalKeyboardKey.select):
-                    const ActivateIntent(),
-              },
-              child: const HomePage(),
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
+        fallbackLocale:
