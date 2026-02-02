58f895075557c07fba5621c1f63255392a58a9d1
Omer I.S. <137101815+omeritzics@users.noreply.github.com>
2026-02-02 20:11:13 +0200

    Update lib/pages/download.dart
    Co-authored-by: qodo-free-for-open-source-projects[bot] <189517486+qodo-free-for-open-source-projects[bot]@users.noreply.github.com>

--- lib/pages/download.dart
+++ lib/pages/download.dart
@@ -2,13 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:updatium/pages/apps.dart';
 
-class DownloadPage extends StatelessWidget {
+class DownloadPage extends StatefulWidget {
   const DownloadPage({super.key});
+
+  @override
+  State<DownloadPage> createState() => _DownloadPageState();
+}
+
+class _DownloadPageState extends State<DownloadPage> {
+  final GlobalKey<AppsPageState> _appsPageKey = GlobalKey<AppsPageState>();
 
   @override
   Widget build(BuildContext context) {
     return AppsPage(
-      key: GlobalKey<AppsPageState>(),
+      key: _appsPageKey,
       initialFilter: AppsFilter(
         onlyNonInstalled: true,
         includeNonInstalled: true,
