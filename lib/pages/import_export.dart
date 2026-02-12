ad6f74b4c90d3d2d362fec6ee915334a3aa4ef27
Omer I.S. <omeritzicschwartz@gmail.com>
2026-02-12 21:28:22 +0200

    Fix

--- lib/pages/import_export.dart
+++ lib/pages/import_export.dart
@@ -402,7 +402,7 @@ class _ImportExportPageState extends State<ImportExportPage> {
                                           runUpdatiumExport(pickOnly: true);
                                         },
                                   icon: const Icon(Icons.folder_open),
-                                  label: Text(
+                                  child: Text(
                                     tr('pickExportDir'),
                                     textAlign: TextAlign.center,
                                   ),
@@ -416,7 +416,7 @@ class _ImportExportPageState extends State<ImportExportPage> {
                                       ? null
                                       : runUpdatiumExport,
                                   icon: const Icon(Icons.upload_file),
-                                  label: Text(
+                                  child: Text(
                                     tr('updatiumExport'),
                                     textAlign: TextAlign.center,
                                   ),
@@ -433,7 +433,7 @@ class _ImportExportPageState extends State<ImportExportPage> {
                                       ? null
                                       : runUpdatiumImport,
                                   icon: const Icon(Icons.download),
-                                  label: Text(
+                                  child: Text(
                                     tr('updatiumImport'),
                                     textAlign: TextAlign.center,
                                   ),
@@ -544,7 +544,7 @@ class _ImportExportPageState extends State<ImportExportPage> {
                                           runSourceSearch(searchSource[0]);
                                         }
                                       },
-                                label: Text(
+                                child: Text(
                                   tr(
                                     'searchX',
                                     args: [lowerCaseIfEnglish(tr('source'))],
@@ -558,13 +558,13 @@ class _ImportExportPageState extends State<ImportExportPage> {
                         FilledButton.icon(
                           onPressed: importInProgress ? null : urlListImport,
                           icon: const Icon(Icons.list_alt),
-                          label: Text(tr('importFromURLList')),
+                          child: Text(tr('importFromURLList')),
                         ),
                         const SizedBox(height: 8),
                         FilledButton.icon(
                           onPressed: importInProgress ? null : runUrlImport,
                           icon: const Icon(Icons.link),
-                          label: Text(tr('importFromURLsInFile')),
+                          child: Text(tr('importFromURLsInFile')),
                         ),
                       ],
                     ),
@@ -580,7 +580,7 @@ class _ImportExportPageState extends State<ImportExportPage> {
                                   runMassSourceImport(source);
                                 },
                           icon: const Icon(Icons.cloud_download),
-                          label: Text(tr('importX', args: [source.name])),
+                          child: Text(tr('importX', args: [source.name])),
                         ),
                       ],
                     ),
@@ -671,7 +671,7 @@ class _ImportErrorDialogState extends State<ImportErrorDialog> {
             Navigator.of(context).pop(null);
           },
           icon: const Icon(Icons.close),
-          label: Text(tr('ok')),
+          child: Text(tr('ok')),
         ),
       ],
     );
@@ -767,7 +767,7 @@ class _SelectionModalState extends State<SelectionModal> {
                   entrySelections.updateAll((key, value) => true);
                 });
               },
-              label: Text(tr('selectAll')),
+              child: Text(tr('selectAll')),
             )
           : TextButton(
               style: const ButtonStyle(visualDensity: VisualDensity.compact),
@@ -776,7 +776,7 @@ class _SelectionModalState extends State<SelectionModal> {
                   entrySelections.updateAll((key, value) => false);
                 });
               },
-              label: Text(tr('deselectX', args: [tr('all')])),
+              child: Text(tr('deselectX', args: [tr('all')])),
             );
     }
 
@@ -954,7 +954,7 @@ class _SelectionModalState extends State<SelectionModal> {
           onPressed: () {
             Navigator.of(context).pop();
           },
-          label: Text(tr('cancel')),
+          child: Text(tr('cancel')),
         ),
         AppTextButton(
           onPressed: entrySelections.values.where((b) => b).isEmpty
@@ -967,7 +967,7 @@ class _SelectionModalState extends State<SelectionModal> {
                         .toList(),
                   );
                 },
-          label: Text(
+          child: Text(
             widget.onlyOneSelectionAllowed
                 ? tr('pick')
                 : tr(
