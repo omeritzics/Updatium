acf94f7fc56f257d312af230060b1539f8784c51
Omer I.S. <omeritzicschwartz@gmail.com>
2026-02-24 17:59:02 +0200

    Fix release build?

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutterVersionCode.toInt()
         versionName = flutterVersionName
     }
