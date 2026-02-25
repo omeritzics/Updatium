1289296725834cfc85937c145cdb194f2829e146
Omer I.S. <omeritzicschwartz@gmail.com>
2026-02-25 20:40:25 +0200

    Fix spacing

--- lib/pages/app.dart
+++ lib/pages/app.dart
@@ -146,6 +146,7 @@ class _AppPageState extends State<AppPage> {
                     context,
                   ).textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold),
                 ),
+                height8,
                 changeLogFn != null || app?.app.releaseDate != null
                     ? GestureDetector(
                         onTap: changeLogFn,
@@ -172,6 +173,7 @@ class _AppPageState extends State<AppPage> {
               ],
             ),
           ),
+          height16,
           Text(
             infoLines,
             textAlign: TextAlign.center,
@@ -246,15 +248,17 @@ class _AppPageState extends State<AppPage> {
                 Column(
                   mainAxisSize: MainAxisSize.min,
                   children: app.certificateHashes.map((hash) {
-                    return GestureDetector(
-                      onLongPress: () {
-                        Clipboard.setData(ClipboardData(text: hash));
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text(tr('copiedToClipboard'))),
-                        );
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 2),
+                      child: GestureDetector(
+                        onLongPress: () {
+                          Clipboard.setData(ClipboardData(text: hash));
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text(tr('copiedToClipboard'))),
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
                           horizontal: 25,
                           vertical: 0,
                         ),
@@ -285,6 +289,9 @@ class _AppPageState extends State<AppPage> {
               }
             },
           ),
+          if (app?.app.additionalSettings['about'] is String &&
+              app?.app.additionalSettings['about'].isNotEmpty)
+            height16,
           if (app?.app.additionalSettings['about'] is String &&
               app?.app.additionalSettings['about'].isNotEmpty)
             Column(
@@ -359,6 +366,7 @@ class _AppPageState extends State<AppPage> {
               ? Theme.of(context).textTheme.displaySmall
               : Theme.of(context).textTheme.displayLarge,
         ),
+        height8,
         Text(
           tr('byX', args: [app?.author ?? tr('unknown')]),
           textAlign: TextAlign.center,
