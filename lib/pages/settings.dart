c63a73408c6a1fbadcc90d79f4a8c46c781c7467
Omer I.S. <omeritzicschwartz@gmail.com>
2026-03-04 19:52:40 +0200

    Fix build

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -8,6 +8,8 @@ import 'package:updatium/components/generated_form.dart';
 import 'package:updatium/components/generated_form_modal.dart';
 import 'package:updatium/custom_errors.dart';
 import 'package:updatium/main.dart';
+import 'package:updatium/pages/apps.dart';
+import 'package:updatium/pages/home.dart';
 import 'package:updatium/providers/apps_provider.dart';
 import 'package:updatium/providers/logs_provider.dart';
 import 'package:updatium/providers/native_provider.dart';
@@ -404,7 +406,7 @@ class _SettingsPageState extends State<SettingsPage> {
         thumbColor: Theme.of(context).colorScheme.primary,
         activeColor: Theme.of(context).colorScheme.primary,
         inactiveColor: Theme.of(context).colorScheme.surfaceContainerHighest,
-        overlayColor: Theme.of(context).colorScheme.primary.withValues(alpha: 0.2),
+        overlayColor: WidgetStateProperty.all(Theme.of(context).colorScheme.primary.withValues(alpha: 0.2)),
         onChanged: (double value) {
           setState(() {
             settingsProvider.updateIntervalSliderVal = value;
