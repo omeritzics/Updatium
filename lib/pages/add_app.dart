cfcb55f7bb8f0ab54ba3b98f04cc46460982aeaa
Omer I.S. <omeritzicschwartz@gmail.com>
2026-04-10 17:44:05 +0300

    Updatium is ready to build (I think?)

--- lib/pages/add_app.dart
+++ lib/pages/add_app.dart
@@ -461,7 +461,7 @@ class AddAppPageState extends State<AddAppPage> {
                     }
                     return MapEntry(
                       e.runtimeType.toString(),
-                      await e.search(searchQuery, querySettings: querySettings),
+                      await e.search(searchQuery, querySettings: querySettings ?? {}),
                     );
                   } catch (err) {
                     if (err is CredsNeededError) {
