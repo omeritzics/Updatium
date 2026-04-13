c7d80bce6dc176bdefb78d44f73810fcf990dde4
Omer I.S. <omeritzicschwartz@gmail.com>
2026-04-13 13:29:57 +0300

    Add missing strings

--- lib/app_sources/github.dart
+++ lib/app_sources/github.dart
@@ -14,6 +14,7 @@ import 'package:url_launcher/url_launcher_string.dart';
 
 class GitHub extends AppSource {
   GitHub({hostChanged = false}) {
+    name = tr('github');
     hosts = ['github.com'];
     appIdInferIsOptional = true;
     showReleaseDateAsVersionToggle = true;
