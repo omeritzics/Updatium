3dfc39d14a94a9c86d36cbe5f33df2e57e45a578
Omer I.S. <omeritzicschwartz@gmail.com>
2026-04-13 20:51:47 +0300

    Commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,6 +4,7 @@ import com.android.build.gradle.internal.api.ApkVariantOutputImpl
 
 plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
