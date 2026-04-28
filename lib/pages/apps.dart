880f4dc1c41170021f11531140009e0bbfb98a00
Omer I.S. <omeritzicschwartz@gmail.com>
2026-04-28 23:29:15 +0300

    Change

--- lib/pages/apps.dart
+++ lib/pages/apps.dart
@@ -1578,7 +1578,7 @@ class AppsPageState extends State<AppsPage> with TickerProviderStateMixin {
             padding: const EdgeInsets.symmetric(horizontal: 16),
             sliver: SliverGrid(
               gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 190,
+                maxCrossAxisExtent: 175,
                 crossAxisSpacing: spacing,
                 mainAxisSpacing: spacing,
                 childAspectRatio: 0.65,
