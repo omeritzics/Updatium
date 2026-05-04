8ff50cdda3ac4014c3d7b5a973113ad7ef40a11c
Omer I.S. <omeritzicschwartz@gmail.com>
2026-05-04 23:52:40 +0300

    refactor(ui, main): replace switch toggles with SwitchListTile and clean up deprecated code
    - Replace Flexible+Switch patterns with SwitchListTile components in settings page
    - Remove deprecated isLocaleRTL function and related tests
    - Clean up background fetch and foreground task related code
    - Update dynamic_system_colors to dynamic_color dependency

--- lib/main.dart
+++ lib/main.dart
@@ -386,6 +386,7 @@ class _UpdatiumState extends State<Updatium> {
                         'versionDetection': true,
                         'apkFilterRegEx': 'fdroid',
                         'invertAPKFilter': true,
+                        'useVersionCodeAsOSVersion': true,
                       },
                       null,
                       false,
