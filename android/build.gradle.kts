283249c0c4849641f04afc8df07c79f9e641454e
Omer I.S. <omeritzicschwartz@gmail.com>
2026-05-04 22:46:15 +0300

    Another

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -23,11 +23,12 @@ subprojects {
     
     val configureAndroid = {
         if (project.hasProperty("android")) {
-            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
-            android.compileSdkVersion(36)
-            
-            // Fix namespace issue for packages that don't specify it
-            if (android is com.android.build.gradle.LibraryExtension) {
+            // AGP 9+ compatible approach - use new API when available
+            try {
+                val android = project.extensions.getByName("android") as com.android.build.gradle.LibraryExtension
+                android.compileSdk = 36
+                
+                // Fix namespace issue for packages that don't specify it
                 if (android.namespace == null) {
                     val manifestFile = file("${project.projectDir}/src/main/AndroidManifest.xml")
                     if (manifestFile.exists()) {
@@ -37,6 +38,28 @@ subprojects {
                             android.namespace = packageMatch.groupValues[1]
                         }
                     }
+                }
+            } catch (e: Exception) {
+                // Fallback for older AGP versions
+                try {
+                    val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
+                    android.compileSdkVersion(36)
+                    
+                    if (android is com.android.build.gradle.LibraryExtension) {
+                        if (android.namespace == null) {
+                            val manifestFile = file("${project.projectDir}/src/main/AndroidManifest.xml")
+                            if (manifestFile.exists()) {
+                                val manifestContent = manifestFile.readText()
+                                val packageMatch = Regex("package=\"([^\"]+)\"").find(manifestContent)
+                                if (packageMatch != null) {
+                                    android.namespace = packageMatch.groupValues[1]
+                                }
+                            }
+                        }
+                    }
+                } catch (fallbackException: Exception) {
+                    // Log but don't fail the build
+                    println("Warning: Could not configure Android extension: ${fallbackException.message}")
                 }
             }
         }
