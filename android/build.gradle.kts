7ae7e8b487a422be15eb767be87c57f3fbba0ced
Omer I.S. <137101815+omeritzics@users.noreply.github.com>
2026-05-06 01:29:32 +0000

    Fix merge conflicts in android/build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,47 +22,6 @@ subprojects {
         project.evaluationDependsOn(":app")
     }
     
-<<<<<<< HEAD
-    // Special handling for android_package_manager plugin for AGP 9 compatibility
-    if (project.name == "android_package_manager") {
-        project.logger.lifecycle("Configuring android_package_manager for AGP 9 compatibility")
-        
-        // Fix manifest by removing package attribute during build
-        project.tasks.whenTaskAdded { task ->
-            if (task.name == "processReleaseManifest" || task.name == "processDebugManifest") {
-                task.doFirst {
-                    val manifestFile = file("${project.projectDir}/src/main/AndroidManifest.xml")
-                    if (manifestFile.exists()) {
-                        val manifestContent = manifestFile.readText()
-                        val fixedContent = manifestContent.replace(
-                            Regex("""package="[^"]*"""), 
-                            ""
-                        )
-                        manifestFile.writeText(fixedContent)
-                        project.logger.lifecycle("Fixed AndroidManifest.xml for android_package_manager")
-                    }
-                }
-            }
-        }
-        
-        project.afterEvaluate {
-            if (project.hasProperty("android")) {
-                try {
-                    val android = project.extensions.getByName("android") as com.android.build.gradle.LibraryExtension
-                    if (android.namespace == null) {
-                        android.namespace = "com.android_package_manager"
-                        project.logger.lifecycle("Set namespace for android_package_manager: ${android.namespace}")
-                    }
-                } catch (e: Exception) {
-                    println("Warning: Could not configure namespace for android_package_manager: ${e.message}")
-                }
-            }
-        }
-        
-=======
-            
->>>>>>> 6057f431 (Revert to AGP 8 and remove submodule approach)
-        return@subprojects
     }
 }
 
