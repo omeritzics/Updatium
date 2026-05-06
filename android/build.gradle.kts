351010bf089a79c15434b12d04c4b2ae652a6d79
Omer I.S. <omeritzicschwartz@gmail.com>
2026-05-06 18:09:49 +0300

    Add namespace configuration for android_package_manager plugin (AGP 8+ compatibility)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -21,6 +21,20 @@ subprojects {
     if (project.path != ":app") {
         project.evaluationDependsOn(":app")
     }
+    
+    // Fix for android_package_manager plugin namespace issue with AGP 8+
+    if (project.name == "android_package_manager") {
+        project.afterEvaluate {
+            if (project.hasProperty("android")) {
+                val androidExtension = project.extensions.findByName("android")
+                if (androidExtension is com.android.build.gradle.LibraryExtension) {
+                    if (androidExtension.namespace == null) {
+                        androidExtension.namespace = "com.android_package_manager"
+                    }
+                }
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
