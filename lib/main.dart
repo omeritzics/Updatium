49649760d2bbe7ef910bc10c19be4c45ca757b51
Omer I.S. <omeritzicschwartz@gmail.com>
2026-05-20 06:55:35 +0300

    Another step towards Slang
    Another step towards Slang
    
    style: auto-fix linting and formatting issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: implementation_imports
+
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
@@ -17,7 +19,6 @@ import 'package:device_info_plus/device_info_plus.dart';
 import 'package:background_fetch/background_fetch.dart';
 import 'package:updatium/services/slang-converter.dart';
 import 'package:simple_localization/src/simple_localization_controller.dart';
-// ignore: implementation_imports
 import 'package:simple_localization/src/localization.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:updatium/services/github_star_prompt.dart';
