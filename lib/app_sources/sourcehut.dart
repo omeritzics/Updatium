58529beb2cb4d71899e8e15a2c84eb5a3a5ec20b
Omer I.S. <omeritzicschwartz@gmail.com>
2026-05-25 09:35:35 +0300

    Commit

--- lib/app_sources/sourcehut.dart
+++ lib/app_sources/sourcehut.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart';
 import 'package:updatium/app_sources/html.dart';
 import 'package:updatium/providers/source_provider.dart';
 import 'package:updatium/providers/source_provider.dart' as source_provider;
+
 import 'package:updatium/services/slang-converter.dart';
 
 class SourceHut extends AppSource {
