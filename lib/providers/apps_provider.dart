79cc92618588cdc79aee7710021e2b13011abbf5
Omer I.S. <omeritzicschwartz@gmail.com>
2026-07-31 05:07:15 +0300

    Define isBg as boolean

--- lib/providers/apps_provider.dart
+++ lib/providers/apps_provider.dart
@@ -634,7 +634,7 @@ class AppsProvider with ChangeNotifier {
 
   Iterable<AppInMemory> getAppValues() => apps.values;
 
-  AppsProvider({isBg = false}) {
+  AppsProvider({bool isBg = false}) {
     // Subscribe to changes in the app foreground status
     foregroundStream = FGBGEvents.instance.stream.asBroadcastStream();
     foregroundSubscription = foregroundStream?.listen((event) async {
